2dcc81d64276bef803b5a5faa449231c8af8dbc1
https://github.com/settings/profile <shashank.agrawal@onmobile.com>
2018-02-10 22:16:25 -0500

    Update context menu load url

--- script/custom/loadcontextmenu.js
+++ script/custom/loadcontextmenu.js
@@ -2,40 +2,43 @@ function loadContextMenu(){
 
 var contextMenu = `<ul id="side-menu" class="nav">
 				<div class="clearfix"></div>`;
-			jQuery.getJSON("https://sportiz.herokuapp.com/context/",
-					function(data) {
-					  	var menus = data;
-					  	var menuArray = menus.menu_items;
-					  	for (var menu in menuArray) {
-							var subContextItems = menuArray[menu].childs;
-							if(subContextItems != null){
-								var menuId = "submenu"+menu;
-								contextMenu += `<li><a href="#" id="btn-1" data-toggle="collapse"
-									data-target="#`+menuId+`" aria-expanded="false"><i
-									class="fa fa-tachometer fa-fw">
-										<div class="icon-bg bg-orange"></div>
-								</i><span class="menu-title">`+menuArray[menu].name+`</a>
-								<ul class="nav collapse" id="`+menuId+`" role="menu"
-									aria-labelledby="btn-1">
-									<li><a></a></li>`;
-								for (var item in subContextItems) {
-									contextMenu += `<li><a href="retrieve?item=` +
-										subContextItems[item].value
-					  				+`">`+ subContextItems[item].name +`</a></li>`
+				$.ajax({
+					  type: 'GET',
+					  url: 'https://sportiz.herokuapp.com/context/',
+					  success: function(data) {
+						  	var menus = data;
+						  	var menuArray = menus.menu_items;
+						  	for (var menu in menuArray) {
+								var subContextItems = menuArray[menu].childs;
+								if(subContextItems != null){
+									var menuId = "submenu"+menu;
+									contextMenu += `<li><a href="#" id="btn-1" data-toggle="collapse"
+										data-target="#`+menuId+`" aria-expanded="false"><i
+										class="fa fa-tachometer fa-fw">
+											<div class="icon-bg bg-orange"></div>
+									</i><span class="menu-title">`+menuArray[menu].name+`</a>
+									<ul class="nav collapse" id="`+menuId+`" role="menu"
+										aria-labelledby="btn-1">
+										<li><a></a></li>`;
+									for (var item in subContextItems) {
+										contextMenu += `<li><a href="retrieve?item=` +
+											subContextItems[item].value
+						  				+`">`+ subContextItems[item].name +`</a></li>`
+									}
+									contextMenu += "</ul>";
+								} else {
+									contextMenu += `<li><a onclick="updateMarkers('` +
+										menuArray[menu].value
+						  				+`');"><i class="`+menuArray[menu].icon+`">
+										<div class="icon-bg bg-primary"></div>
+										</i><span class="menu-title">` 
+										+ menuArray[menu].name +
+										`</span></a>`;
 								}
-								contextMenu += "</ul>";
-							} else {
-								contextMenu += `<li><a onclick="updateMarkers('` +
-									menuArray[menu].value
-					  				+`');"><i class="`+menuArray[menu].icon+`">
-									<div class="icon-bg bg-primary"></div>
-									</i><span class="menu-title">` 
-									+ menuArray[menu].name +
-									`</span></a>`;
-							}
-							contextMenu += "</li>";
-					  	}
-					  	contextMenu += "</ul>";
-					  	document.getElementById("context-menu").innerHTML = contextMenu;
+								contextMenu += "</li>";
+						  	}
+						  	contextMenu += "</ul>";
+						  	document.getElementById("context-menu").innerHTML = contextMenu;
+					}
 				});
 }
